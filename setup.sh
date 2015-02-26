199022ac8056b99126adbe276356996626f8f1ed
zalky <zalan.k@gmail.com>
2015-02-26 15:58:18 -0500

    Updated setup.sh.

--- setup.sh
+++ setup.sh
@@ -17,14 +17,14 @@ local/share/elisp"
 
 # Simlink to basic configuration files
 for TARGET in $TARGETLIST ; do
-    if [[ -e $HOME/$TARGET ]] || [[ -h $HOME/$TARGET ]] ; then
+    if [[ -e $HOME/$TARGET || -h $HOME/$TARGET ]] ; then
         echo "Warning, $HOME/$TARGET already exists, \
-backing up as $TARGET.`date +%Y%m%d-%H.%M.%S`.setup.bak"
+backing up as $TARGET.$(date +%Y%m%d-%H.%M.%S).setup.bak"
         mv $HOME/$TARGET $HOME/$TARGET.`date "+%Y%m%d-%H.%M.%S"`.setup.bak
     fi
     BASENAME=${TARGET##*/}
     NODOT=${BASENAME#.}
-    ln -sf `pwd`/setupfiles/$NODOT $HOME/$TARGET
+    ln -sf $(pwd)/setupfiles/$NODOT $HOME/$TARGET
 done
 
 exit 0
