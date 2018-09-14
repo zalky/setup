c45ec995fbd5bdf9ffd03fb4a81a4186a9a89378
zalky <zalan.k@gmail.com>
2018-09-14 16:23:05 -0400

    Forgot paredit config

---
+++ dotfiles/.emacs.conf/paredit-config.el
@@ -0,0 +1,24 @@
+
+;; Require smartparens just for `sp-unwrap-sexp`
+(require 'smartparens)
+
+(require 'paredit)
+
+(add-hook 'lisp-mode-hook #'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'cider-repl-mode-hook #'paredit-mode)
+
+(define-key paredit-mode-map (kbd "M-;") 'comment-dwim)
+(define-key paredit-mode-map (kbd "C-)") 'paredit-forward-slurp-sexp)
+(define-key paredit-mode-map (kbd "C-(") 'paredit-forward-barf-sexp)
+(define-key paredit-mode-map (kbd "C-M-(") 'paredit-backward-slurp-sexp)
+(define-key paredit-mode-map (kbd "C-M-)") 'paredit-backward-barf-sexp)
+(define-key paredit-mode-map (kbd "C-M-<backspace>") 'sp-unwrap-sexp)
+(define-key paredit-mode-map (kbd "C-M-S-<backspace>") 'paredit-splice-sexp)
+(define-key paredit-mode-map (kbd "C-M-i") 'paredit-forward-down)
+(define-key paredit-mode-map (kbd "C-M-u") 'paredit-backward-up)
+(define-key paredit-mode-map (kbd "C-M-o") 'paredit-backward-down)
+(define-key paredit-mode-map (kbd "C-M-p") 'paredit-forward-up)
+
+
